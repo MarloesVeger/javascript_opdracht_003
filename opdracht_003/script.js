1af139cb596841c0f6a4779e790526d7354ac66c
MarloesVeger <89524035+MarloesVeger@users.noreply.github.com>
2021-09-19 16:36:12 +0200

    Add files via upload

---
+++ opdracht_003/script.js
@@ -0,0 +1,18 @@
+const age = 18;
+if (age > 17) {
+    console.log("You are 18 years or older and allowed to enter");
+} else {
+    console.log("You are under 18, please do not enter!");
+}
+const isFemale = false;
+if (isFemale) {
+    console.log("This is a lady");
+} else {
+    console.log("This is not a lady");
+}
+const driverStatus = 'bob';
+if (bob = true) {
+    console.log("Driver has status BOB and is allowed to drive");
+} else {
+    console.log("Driver has no BOB status ans is not allowed to drive")
+}
